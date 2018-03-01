c8399ec90ba99e9b251c4ed3b0840555c59dc1f1
Yuriy Denysov <ydenysov@scholastic.com>
2018-03-01 16:51:36 -0500

    ayyy it works

--- youtube_to_mp3.py
+++ youtube_to_mp3.py
@@ -17,40 +17,40 @@ class Track:
         self.title = title
         self.artist = artist
 
-f = open("./songs.txt", "r").read
-words = f().split()
-dlTrack = Track(words[0], words[1], words[2])
-print(dlTrack.url)
-print(dlTrack.title)
-print(dlTrack.artist)
+f = open("./songs.txt", "r").readlines()
+for l in f:
+    words = l.split()
+    dlTrack = Track(words[0], words[1], words[2])
+    print(dlTrack.url)
+    print(dlTrack.title)
+    print(dlTrack.artist)
 
+    options = {
+        'format':'bestaudio/best',
+        'extractaudio':True,
+        'audioformat':'mp3',
+        'addmetadata':True,
+        'outtmpl': u'%(title)s.%(ext)s',
+        'noplaylist':True,
+        'nocheckcertificate':True,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }]
+    }
 
-options = {
-    'format':'bestaudio/best',
-    'extractaudio':True,
-    'audioformat':'mp3',
-    'addmetadata':True,
-    'outtmpl': u'%(title)s.%(ext)s',
-    'noplaylist':True,
-    'nocheckcertificate':True,
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }]
-}
-
-with youtube_dl.YoutubeDL(options) as ydl:
-    result = ydl.extract_info("{}".format(dlTrack.url))
-    filename = ydl.prepare_filename(result)
-    if "youtube" in url:
-        filename = ydl.prepare_filename(result)[:-5] + '.mp3'
-    if "soundcloud" in url:
+    with youtube_dl.YoutubeDL(options) as ydl:
+        result = ydl.extract_info("{}".format(dlTrack.url))
         filename = ydl.prepare_filename(result)
-        metatag = mutagen.File(filename)
-        metatag.add_tags()
-        metatag.save(filename, v1=2)
-    metatag = EasyID3(filename)
-    metatag['title'] = "{}".format(dlTrack.title)
-    metatag['artist'] = "{}".format(dlTrack.artist)
-    metatag.save()
+        if "youtube" in dlTrack.url:
+            filename = ydl.prepare_filename(result)[:-5] + '.mp3'
+        if "soundcloud" in dlTrack.url:
+            filename = ydl.prepare_filename(result)
+            metatag = mutagen.File(filename)
+            metatag.add_tags()
+            metatag.save(filename, v1=2)
+        metatag = EasyID3(filename)
+        metatag['title'] = "{}".format(dlTrack.title)
+        metatag['artist'] = "{}".format(dlTrack.artist)
+        metatag.save()
